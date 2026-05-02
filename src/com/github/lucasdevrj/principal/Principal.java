1817f6337b671cc0e4d2b9f54c0206ab102cb2ee
Lucas <lucaspereiradelima2020@gmail.com>
2026-05-02 20:02:46 -0300

    Calculando as comissões dos funcionários e as exibindo em vários tipos de laços.

--- src/com/github/lucasdevrj/principal/Principal.java
+++ src/com/github/lucasdevrj/principal/Principal.java
@@ -1,16 +1,72 @@
 package com.github.lucasdevrj.principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
         String[] vendedores = {"Carlos", "Mariana", "João", "Fernanda"};
         double[] vendas = {4000.00, 8000.00, 12000.00, 5000.00};
 
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Testando com o For:");
         for (int i = 0; i < vendedores.length; i++) {
             String vendedor = vendedores[i];
             double totalDeVendas = calcularComissao(vendas[i]);
             System.out.printf("%s - comissão %.2f \n", vendedor, totalDeVendas);
         }
+
+        System.out.println("Testando com o While:");
+        int i = 0;
+        boolean desejaExibirVendedor = true;
+        while (desejaExibirVendedor && i < vendedores.length) {
+            String vendedor = vendedores[i];
+            double totalDeVendas = calcularComissao(vendas[i]);
+            System.out.printf("%s - comissão %.2f \n", vendedor, totalDeVendas);
+            i++;
+            System.out.print("Deseja exibir outro vendedor?");
+            desejaExibirVendedor = entrada.nextBoolean();
+        }
+
+        System.out.println("Testando com o Do While:");
+        i = 0;
+        do {
+            String vendedor = vendedores[i];
+            double totalDeVendas = calcularComissao(vendas[i]);
+            System.out.printf("%s - comissão %.2f \n", vendedor, totalDeVendas);
+            i++;
+            System.out.print("Deseja exibir outro vendedor?");
+            desejaExibirVendedor = entrada.nextBoolean();
+        } while (desejaExibirVendedor && i < vendedores.length);
+
+        System.out.println("Testando com o For mais condicionais:");
+        System.out.println("Todos os funcionários antes da Mariana");
+        for (int j = 0; j < vendedores.length; j++) {
+            String vendedor = vendedores[j];
+            double totalDeVendas = calcularComissao(vendas[j]);
+            if (vendedor.equalsIgnoreCase("mariana")) {
+                break;
+            }
+            System.out.printf("%s - comissão %.2f \n", vendedor, totalDeVendas);
+        }
+
+        System.out.println("Todos os funcionários menos a Mariana");
+        for (int j = 0; j < vendedores.length; j++) {
+            String vendedor = vendedores[j];
+            double totalDeVendas = calcularComissao(vendas[j]);
+            if (vendedor.equalsIgnoreCase("mariana")) {
+                continue;
+            }
+            System.out.printf("%s - comissão %.2f \n", vendedor, totalDeVendas);
+        }
+
+        System.out.println("Usando o ForEach:");
+        for (String vendedor : vendedores) {
+            System.out.println("Vendedor = " + vendedor);
+        }
+
+        entrada.close();
     }
 
     public static double calcularComissao(double totalVendas) {
